908291dc304e212fbcacc45378201d892e16aedd
sgalaz <sgalaz@adentu.cl>
2021-03-17 08:40:23 -0300

    nearest neighbor example

--- scratchs/nn.py
+++ scratchs/nn.py
@@ -11,4 +11,19 @@ class NearestNeighbor:
 		self.ytr = y
 
 	def predict(self, X):
-		"""
+		""" X is N x D where each row is an example. Y is 1-dimension of size N """
+		num_test = X.shape[0]
+		# lets make sure that the output type matches the input type
+		Ypred = np.zeros(num_test, dtype = self.ytr.dtype)
+
+		# loop over all test rows
+		for i in xrange(num_test):
+			# find the nearest training image to the i'th test image
+			# using L1 distance (sum of absolute value differences)
+			distances = np.sum(np.abs(self.Xtr - X[i,:]), axis = 1)
+			min_index = np.argmin(distances) # get the index with smallest distance
+			Ypred[i] = self.ytr[min_index] # predict the label of the nearest example
+
+		return Ypred
+
+
