b18096a6d21fa2499104cbcef37532f57ba5849f
sigalaz <sigalaz@miuandes.cl>
2022-04-07 13:53:28 -0400

    minor chages to knn

--- A1/knn.py
+++ A1/knn.py
@@ -1,6 +1,7 @@
 """
 Implements a K-Nearest Neighbor classifier in PyTorch.
 """
+from distutils import dist
 import torch
 import statistics
 
@@ -14,206 +15,209 @@ def hello():
 
 
 def compute_distances_two_loops(x_train, x_test):
-  """
-  Computes the squared Euclidean distance between each element of the training
-  set and each element of the test set. Images should be flattened and treated
-  as vectors.
+    """
+    Computes the squared Euclidean distance between each element of the training
+    set and each element of the test set. Images should be flattened and treated
+    as vectors.
 
-  This implementation uses a naive set of nested loops over the training and
-  test data.
+    This implementation uses a naive set of nested loops over the training and
+    test data.
 
-  The input data may have any number of dimensions -- for example this function
-  should be able to compute nearest neighbor between vectors, in which case
-  the inputs will have shape (num_{train, test}, D); it should alse be able to
-  compute nearest neighbors between images, where the inputs will have shape
-  (num_{train, test}, C, H, W). More generally, the inputs will have shape
-  (num_{train, test}, D1, D2, ..., Dn); you should flatten each element
-  of shape (D1, D2, ..., Dn) into a vector of shape (D1 * D2 * ... * Dn) before
-  computing distances.
+    The input data may have any number of dimensions -- for example this function
+    should be able to compute nearest neighbor between vectors, in which case
+    the inputs will have shape (num_{train, test}, D); it should also be able to
+    compute nearest neighbors between images, where the inputs will have shape
+    (num_{train, test}, C, H, W). More generally, the inputs will have shape
+    (num_{train, test}, D1, D2, ..., Dn); you should flatten each element
+    of shape (D1, D2, ..., Dn) into a vector of shape (D1 * D2 * ... * Dn) before
+    computing distances.
 
-  The input tensors should not be modified.
+    The input tensors should not be modified.
 
-  NOTE: Your implementation may not use `torch.norm`, `torch.dist`,
-  `torch.cdist`, or their instance method variants x.norm / x.dist / x.cdist.
-  You may not use any functions from torch.nn or torch.nn.functional.
+    NOTE: Your implementation may not use `torch.norm`, `torch.dist`,
+    `torch.cdist`, or their instance method variants x.norm / x.dist / x.cdist.
+    You may not use any functions from torch.nn or torch.nn.functional.
 
-  Inputs:
-  - x_train: Torch tensor of shape (num_train, D1, D2, ...)
-  - x_test: Torch tensor of shape (num_test, D1, D2, ...)
+    Inputs:
+    - x_train: Torch tensor of shape (num_train, D1, D2, ...)
+    - x_test: Torch tensor of shape (num_test, D1, D2, ...)
 
-  Returns:
-  - dists: Torch tensor of shape (num_train, num_test) where dists[i, j] is the
-    squared Euclidean distance between the ith training point and the jth test
-    point. It should have the same dtype as x_train.
-  """
-  # Initialize dists to be a tensor of shape (num_train, num_test) with the
-  # same datatype and device as x_train
-  num_train = x_train.shape[0]
-  num_test = x_test.shape[0]
-  dists = x_train.new_zeros(num_train, num_test)
-  ##############################################################################
-  # TODO: Implement this function using a pair of nested loops over the        #
-  # training data and the test data.                                           #
-  #                                                                            #
-  # You may not use torch.norm (or its instance method variant), nor any       #
-  # functions from torch.nn or torch.nn.functional.                            #
-  ##############################################################################
-  # Replace "pass" statement with your code
-  x_train_flattenned = x_train.view(num_train, -1)
-  x_test_flattened = x_test.view(num_test, -1)
+    Returns:
+    - dists: Torch tensor of shape (num_train, num_test) where dists[i, j] is the
+      squared Euclidean distance between the ith training point and the jth test
+      point. It should have the same dtype as x_train.
+    """
+    # Initialize dists to be a tensor of shape (num_train, num_test) with the
+    # same datatype and device as x_train
+    num_train = x_train.shape[0]
+    num_test = x_test.shape[0]
+    dists = x_train.new_zeros(num_train, num_test)
+    ##############################################################################
+    # TODO: Implement this function using a pair of nested loops over the        #
+    # training data and the test data.                                           #
+    #                                                                            #
+    # You may not use torch.norm (or its instance method variant), nor any       #
+    # functions from torch.nn or torch.nn.functional.                            #
+    ##############################################################################
+    # Replace "pass" statement with your code
+    x_train_flattenned = x_train.view(num_train, -1)
+    x_test_flattened = x_test.view(num_test, -1)
 
-  for i, train_sample in enumerate(x_train_flattenned):
-  	for j, test_sample in enumerate(x_test_flattened):
-  		dists[i,j] = torch.sqrt(torch.sum((test_sample - train_sample)**2))
+    for i, train_sample in enumerate(x_train_flattenned):
+      for j, test_sample in enumerate(x_test_flattened):
+        dists[i,j] = torch.sqrt(torch.sum((test_sample - train_sample)**2))
 
+
+    return dists
 
   ##############################################################################
   #                             END OF YOUR CODE                               #
   ##############################################################################
-  return dists
+  
 
 
 def compute_distances_one_loop(x_train, x_test):
-  """
-  Computes the squared Euclidean distance between each element of the training
-  set and each element of the test set. Images should be flattened and treated
-  as vectors.
+    """
+    Computes the squared Euclidean distance between each element of the training
+    set and each element of the test set. Images should be flattened and treated
+    as vectors.
 
-  This implementation uses only a single loop over the training data.
+    This implementation uses only a single loop over the training data.
 
-  Similar to compute_distances_two_loops, this should be able to handle inputs
-  with any number of dimensions. The inputs should not be modified.
+    Similar to compute_distances_two_loops, this should be able to handle inputs
+    with any number of dimensions. The inputs should not be modified.
 
-  NOTE: Your implementation may not use `torch.norm`, `torch.dist`,
-  `torch.cdist`, or their instance method variants x.norm / x.dist / x.cdist.
-  You may not use any functions from torch.nn or torch.nn.functional.
+    NOTE: Your implementation may not use `torch.norm`, `torch.dist`,
+    `torch.cdist`, or their instance method variants x.norm / x.dist / x.cdist.
+    You may not use any functions from torch.nn or torch.nn.functional.
 
-  Inputs:
-  - x_train: Torch tensor of shape (num_train, D1, D2, ...)
-  - x_test: Torch tensor of shape (num_test, D1, D2, ...)
+    Inputs:
+    - x_train: Torch tensor of shape (num_train, D1, D2, ...)
+    - x_test: Torch tensor of shape (num_test, D1, D2, ...)
 
-  Returns:
-  - dists: Torch tensor of shape (num_train, num_test) where dists[i, j] is the
-    squared Euclidean distance between the ith training point and the jth test
-    point.
-  """
-  # Initialize dists to be a tensor of shape (num_train, num_test) with the
-  # same datatype and device as x_train
-  num_train = x_train.shape[0]
-  num_test = x_test.shape[0]
-  dists = x_train.new_zeros(num_train, num_test)
-  ##############################################################################
-  # TODO: Implement this function using only a single loop over x_train.       #
-  #                                                                            #
-  # You may not use torch.norm (or its instance method variant), nor any       #
-  # functions from torch.nn or torch.nn.functional.                            #
-  ##############################################################################
-  # Replace "pass" statement with your code
-  x_train_flattenned = x_train.view(num_train, -1)
-  x_test_flattened   = x_test.view(num_test, -1)
+    Returns:
+    - dists: Torch tensor of shape (num_train, num_test) where dists[i, j] is the
+      squared Euclidean distance between the ith training point and the jth test
+      point.
+    """
+    # Initialize dists to be a tensor of shape (num_train, num_test) with the
+    # same datatype and device as x_train
+    num_train = x_train.shape[0]
+    num_test = x_test.shape[0]
+    dists = x_train.new_zeros(num_train, num_test)
+    ##############################################################################
+    # TODO: Implement this function using only a single loop over x_train.       #
+    #                                                                            #
+    # You may not use torch.norm (or its instance method variant), nor any       #
+    # functions from torch.nn or torch.nn.functional.                            #
+    ##############################################################################
+    # Replace "pass" statement with your code
+    x_train_flattenned = x_train.view(num_train, -1)
+    x_test_flattened   = x_test.view(num_test, -1)
 
-  for i, train_sample in enumerate(x_train_flattenned):
-  	dists[i, :] = torch.sqrt((torch.sum(x_test_flattened - train_sample.t(), dim=1)**2))
-  	
+    print(x_train_flattenned)
+    print(x_test_flattened)
 
-  
-  	
+    for i, train_sample in enumerate(x_train_flattenned):
+      dists[i, :] = torch.sqrt((torch.sum(x_test_flattened - train_sample(), dim=1)**2))
+      
+    return dists
+	
 
-  #print(p.shape)
+    #print(p.shape)
 
-  ##############################################################################
-  #                             END OF YOUR CODE                               #
-  ##############################################################################
-  return dists
-
+    ##############################################################################
+    #                             END OF YOUR CODE                               #
+    ##############################################################################
+    
 
 def compute_distances_no_loops(x_train, x_test):
-  """
-  Computes the squared Euclidean distance between each element of the training
-  set and each element of the test set. Images should be flattened and treated
-  as vectors.
+    """
+    Computes the squared Euclidean distance between each element of the training
+    set and each element of the test set. Images should be flattened and treated
+    as vectors.
 
-  This implementation should not use any Python loops. For memory-efficiency,
-  it also should not create any large intermediate tensors; in particular you
-  should not create any intermediate tensors with O(num_train*num_test)
-  elements.
+    This implementation should not use any Python loops. For memory-efficiency,
+    it also should not create any large intermediate tensors; in particular you
+    should not create any intermediate tensors with O(num_train*num_test)
+    elements.
 
-  Similar to compute_distances_two_loops, this should be able to handle inputs
-  with any number of dimensions. The inputs should not be modified.
+    Similar to compute_distances_two_loops, this should be able to handle inputs
+    with any number of dimensions. The inputs should not be modified.
 
-  NOTE: Your implementation may not use `torch.norm`, `torch.dist`,
-  `torch.cdist`, or their instance method variants x.norm / x.dist / x.cdist.
-  You may not use any functions from torch.nn or torch.nn.functional.
-  Inputs:
-  - x_train: Torch tensor of shape (num_train, C, H, W)
-  - x_test: Torch tensor of shape (num_test, C, H, W)
+    NOTE: Your implementation may not use `torch.norm`, `torch.dist`,
+    `torch.cdist`, or their instance method variants x.norm / x.dist / x.cdist.
+    You may not use any functions from torch.nn or torch.nn.functional.
+    Inputs:
+    - x_train: Torch tensor of shape (num_train, C, H, W)
+    - x_test: Torch tensor of shape (num_test, C, H, W)
 
-  Returns:
-  - dists: Torch tensor of shape (num_train, num_test) where dists[i, j] is the
-    squared Euclidean distance between the ith training point and the jth test
-    point.
-  """
-  # Initialize dists to be a tensor of shape (num_train, num_test) with the
-  # same datatype and device as x_train
-  num_train = x_train.shape[0]
-  num_test = x_test.shape[0]
-  dists = x_train.new_zeros(num_train, num_test)
-  ##############################################################################
-  # TODO: Implement this function without using any explicit loops and without #
-  # creating any intermediate tensors with O(num_train * num_test) elements.   #
-  #                                                                            #
-  # You may not use torch.norm (or its instance method variant), nor any       #
-  # functions from torch.nn or torch.nn.functional.                            #
-  #                                                                            #
-  # HINT: Try to formulate the Euclidean distance using two broadcast sums     #
-  #       and a matrix multiply.                                               #
-  ##############################################################################
-  # Replace "pass" statement with your code
-  pass
-  ##############################################################################
-  #                             END OF YOUR CODE                               #
-  ##############################################################################
-  return dists
+    Returns:
+    - dists: Torch tensor of shape (num_train, num_test) where dists[i, j] is the
+      squared Euclidean distance between the ith training point and the jth test
+      point.
+    """
+    # Initialize dists to be a tensor of shape (num_train, num_test) with the
+    # same datatype and device as x_train
+    num_train = x_train.shape[0]
+    num_test = x_test.shape[0]
+    dists = x_train.new_zeros(num_train, num_test)
+    ##############################################################################
+    # TODO: Implement this function without using any explicit loops and without #
+    # creating any intermediate tensors with O(num_train * num_test) elements.   #
+    #                                                                            #
+    # You may not use torch.norm (or its instance method variant), nor any       #
+    # functions from torch.nn or torch.nn.functional.                            #
+    #                                                                            #
+    # HINT: Try to formulate the Euclidean distance using two broadcast sums     #
+    #       and a matrix multiply.                                               #
+    ##############################################################################
+    # Replace "pass" statement with your code
+    pass
+    ##############################################################################
+    #                             END OF YOUR CODE                               #
+    ##############################################################################
+    return dists
 
 
 def predict_labels(dists, y_train, k=1):
-  """
-  Given distances between all pairs of training and test samples, predict a
-  label for each test sample by taking a **majority vote** among its k nearest
-  neighbors in the training set.
+    """
+    Given distances between all pairs of training and test samples, predict a
+    label for each test sample by taking a **majority vote** among its k nearest
+    neighbors in the training set.
 
-  In the event of a tie, this function **should** return the smallest label. For
-  example, if k=5 and the 5 nearest neighbors to a test example have labels
-  [1, 2, 1, 2, 3] then there is a tie between 1 and 2 (each have 2 votes), so
-  we should return 1 since it is the smallest label.
+    In the event of a tie, this function **should** return the smallest label. For
+    example, if k=5 and the 5 nearest neighbors to a test example have labels
+    [1, 2, 1, 2, 3] then there is a tie between 1 and 2 (each have 2 votes), so
+    we should return 1 since it is the smallest label.
 
-  This function should not modify any of its inputs.
+    This function should not modify any of its inputs.
 
-  Inputs:
-  - dists: Torch tensor of shape (num_train, num_test) where dists[i, j] is the
-    squared Euclidean distance between the ith training point and the jth test
-    point.
-  - y_train: Torch tensor of shape (num_train,) giving labels for all training
-    samples. Each label is an integer in the range [0, num_classes - 1]
-  - k: The number of nearest neighbors to use for classification.
+    Inputs:
+    - dists: Torch tensor of shape (num_train, num_test) where dists[i, j] is the
+      squared Euclidean distance between the ith training point and the jth test
+      point.
+    - y_train: Torch tensor of shape (num_train,) giving labels for all training
+      samples. Each label is an integer in the range [0, num_classes - 1]
+    - k: The number of nearest neighbors to use for classification.
 
-  Returns:
-  - y_pred: A torch int64 tensor of shape (num_test,) giving predicted labels
-    for the test data, where y_pred[j] is the predicted label for the jth test
-    example. Each label should be an integer in the range [0, num_classes - 1].
-  """
-  num_train, num_test = dists.shape
-  y_pred = torch.zeros(num_test, dtype=torch.int64)
-  ##############################################################################
-  # TODO: Implement this function. You may use an explicit loop over the test  #
-  # samples. Hint: Look up the function torch.topk                             #
-  ##############################################################################
-  # Replace "pass" statement with your code
-  pass
-  ##############################################################################
-  #                             END OF YOUR CODE                               #
-  ##############################################################################
-  return y_pred
+    Returns:
+    - y_pred: A torch int64 tensor of shape (num_test,) giving predicted labels
+      for the test data, where y_pred[j] is the predicted label for the jth test
+      example. Each label should be an integer in the range [0, num_classes - 1].
+    """
+    num_train, num_test = dists.shape
+    y_pred = torch.zeros(num_test, dtype=torch.int64)
+    ##############################################################################
+    # TODO: Implement this function. You may use an explicit loop over the test  #
+    # samples. Hint: Look up the function torch.topk                             #
+    ##############################################################################
+    # Replace "pass" statement with your code
+    pass
+    ##############################################################################
+    #                             END OF YOUR CODE                               #
+    ##############################################################################
+    return y_pred
 
 
 class KnnClassifier:
